e2d3c55540873618e6f92041e2ac96846cc8201a
Hotehama2022 <hotehama-r@cin-group.com>
2025-01-16 23:20:33 +0900

    クラスの練習

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,15 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 1000;
+		double height = 160.2;
+		int num1 = 1;
+		int num2 = 50;
+		
+		GeekIntroduction.Greeting(age, height);
+		GeekIntroduction.SpecialSkill(num1, num2);
+	}
+
+}
